1c6599853fd816e816e0367d2dbbe86a13d8b2d7
Kevin Bell <bellKevin@pm.me>
2022-05-17 20:49:06 +0000

    Update SubstringRetrieval.java

--- substringRetrieval/src/substringretrieval/SubstringRetrieval.java
+++ substringRetrieval/src/substringretrieval/SubstringRetrieval.java
@@ -1,11 +1,8 @@
 package substringretrieval;
 
-import java.util.Scanner;
-
 public class SubstringRetrieval {
     
     public static void main(String[] args) {
-        Scanner computerKeyboardInput = new Scanner(System.in);
         String candide = "we must cultivate our garden";
         System.out.println(candide.substring(8));
         System.out.println(candide.substring(3, 17));
