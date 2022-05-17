6ff54aebbe839a84bd9e307dbe456f8bc5b6f422
Kevin Bell <bellKevin@pm.me>
2022-05-17 20:46:52 +0000

    Update SubstringRetrieval.java

--- substringRetrieval/src/substringretrieval/SubstringRetrieval.java
+++ substringRetrieval/src/substringretrieval/SubstringRetrieval.java
@@ -1,25 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package substringretrieval;
-import java.util.Scanner;
-/**
- *
- * @author 4800590195
- */
-public class SubstringRetrieval {
 
-    /**
-     * @param args the command line arguments
-     */
+import java.util.Scanner;
+
+public class SubstringRetrieval {
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner computerKeyboardInput = new Scanner(System.in);
         String candide = "we must cultivate our garden";
         System.out.println(candide.substring(8));
         System.out.println(candide.substring(3, 17));
-    }
-    
+    }  
 }
